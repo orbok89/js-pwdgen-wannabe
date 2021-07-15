c07ce6b3c464f6f949835142d3049591039927cc
BigFelk <fedferrara89@gmail.com>
2021-07-15 09:47:03 +0200

    corretto finito

--- js/main.js
+++ js/main.js
@@ -2,5 +2,4 @@ var nome = prompt ('dimmi nome');
 var cognome = prompt ('dimmi cognome');
 var colore = prompt ('dimmi colore');
 var pwd = nome+cognome+colore+'21';
-alert(pwd);
-
+document.writeln(pwd);
